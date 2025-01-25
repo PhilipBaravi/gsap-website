7bd4374a37204887fa470de9db98d88599afe3f1
Philip <f.baravi121@gmail.com>
2025-01-25 19:42:45 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724FF",
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66",
+        },
+      },
+      fontFamily: {
+        "circular-web": ["circular-web", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robet-regular", "sans-serif"],
+        zentry: ["zentry", "sans-serif"],
       },
     },
   },
