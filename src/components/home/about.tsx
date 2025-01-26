094868219ec7d8a337292ce0de7e9792bb32589b
Philip <f.baravi121@gmail.com>
2025-01-26 18:24:58 +0100

    Finish about section

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { FC, useEffect } from "react";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import Image from "next/image";
+
+import AnimatedTitle from "./animate-title";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const About = () => {
-  useGSAP(() => {
+const About: FC = () => {
+  // GSAP Animation
+  useEffect(() => {
     const clipAnimation = gsap.timeline({
       scrollTrigger: {
         trigger: "#clip",
@@ -24,7 +28,11 @@ const About = () => {
       height: "100vh",
       borderRadius: 0,
     });
-  });
+
+    return () => {
+      ScrollTrigger.killAll();
+    };
+  }, []);
 
   return (
     <div id="about" className="min-h-screen w-screen">
@@ -32,6 +40,11 @@ const About = () => {
         <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
@@ -43,11 +56,14 @@ const About = () => {
       </div>
 
       <div className="h-dvh w-screen" id="clip">
-        <div className="mask-clip-path about-image">
-          <img
-            src="img/about.webp"
+        <div className="mask-clip-path about-image relative">
+          <Image
+            src="/img/about.webp" // Path to your image in the public folder
             alt="Background"
+            layout="fill" // Makes the image fill the parent container
+            objectFit="cover" // Ensures the image maintains aspect ratio
             className="absolute left-0 top-0 size-full object-cover"
+            priority // Ensures the image loads faster as it's above the fold
           />
         </div>
       </div>
