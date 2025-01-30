e0addc5103a523b02fa746243935f13ca4fafec9
Philip <f.baravi121@gmail.com>
2025-01-30 19:29:11 +0100

    Finish features component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/home/hero";
 import About from "@/components/home/about";
 import Navbar from "@/components/home/navbar";
 import Features from "@/components/home/features";
+import Story from "@/components/home/story";
 
 export default function Home() {
   return (
---
+++ src/components/home/story.tsx
@@ -0,0 +1,5 @@
+const Story = () => {
+  return <section></section>;
+};
+
+export default Story;
